e98ed1c12713f23aec12c12d125acb6ab608735c
Rias <hello@rias.be>
2020-07-10 15:33:23 +0200

    no http client in L6

--- app/Http/Front/Controllers/SubscribeToEmailListController.php
+++ app/Http/Front/Controllers/SubscribeToEmailListController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Front\Controllers;
 
 use App\Http\Front\Requests\SubscribeToEmailListRequest;
+use Exception;
+use GuzzleHttp\Client;
 
 class SubscribeToEmailListController
 {
     public function __invoke(SubscribeToEmailListRequest $request)
     {
-        $response = Http::post('https://spatie.be/mailcoach/subscribe/4af46b59-3784-41a5-9272-6da31afa3a02', [
+        $client = new Client();
+
+        $response = $client->post('https://spatie.be/mailcoach/subscribe/4af46b59-3784-41a5-9272-6da31afa3a02', [
             'email' => $request->email,
             'tags' => 'medialibrary-pro',
         ]);
 
-        if (! $response->successful()) {
+        if ($response->getStatusCode() !== 201 || $response->getStatusCode() !== 200) {
             throw new Exception('Could not subscribe');
         }
 
