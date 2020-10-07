3f103e68469b8c1b832523d448294523a0259d73
Willem Van Bockstal <willem@spatie.be>
2020-10-07 17:59:51 +0200

    Layout layout layout

--- resources/views/partials/button.blade.php
+++ resources/views/partials/button.blade.php
@@ -8,7 +8,7 @@
     <span 
         style="top:4px; left: 4px;"
         class="pointer-events-none absolute pr-2-em h-full w-full flex items-center justify-end rounded-sm transform translate-x-0 group-hover:translate-x-5-em transition-transform duration-200
-        text-inherit border-blue-900 border-opacity-25 border-4 bg-blue-400 bg-opacity-25
+        text-inherit border-transparent border-4 bg-blue-600 bg-opacity-25
     ">
         <i style="font-size: .75em" class="far fa-angle-right opacity-0 group-hover:opacity-50 transition-opacity duration-100"></i> 
     </span>    
