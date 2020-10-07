11820e188f7064065cdb112c71ad82681f1dc21a
freek <freek@spatie.be>
2020-10-07 17:38:43 +0200

    wip

--- app/Http/Front/Controllers/Demo/AttachmentDemoController.php
+++ app/Http/Front/Controllers/Demo/AttachmentDemoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Front\Controllers\Demo;
 
+use App\Http\Front\Requests\Demo\AttachmentDemoRequest;
+
 class AttachmentDemoController
 {
     public function create()
@@ -9,7 +11,19 @@ class AttachmentDemoController
         return view('front.demo.attachment');
     }
 
-    public function store()
+    public function store(AttachmentDemoRequest $request)
     {
+        /**
+         * We're not going to persist in this demo.
+         *
+         * This is how you would normally handle the upload
+         */
+//        $yourModel
+//            ->addFromMediaLibraryRequest($request->media)
+//            ->toMediaCollection('images');
+
+        flash()->success('Your form has been submitted');
+
+        return back();
     }
 }
