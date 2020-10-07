c5fce0967862046e38ff9bbedde5539e44832942
Laravel Shift <shift@laravelshift.com>
2020-10-07 13:37:22 +0000

    Namespace seeders

--- database/seeders/ReleaseSeeder.php
+++ database/seeders/ReleaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\Release;
 use Illuminate\Database\Seeder;
 
--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Console\UpdateVideosCommand;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
