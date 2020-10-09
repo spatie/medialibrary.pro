b9569e6f60059fc6a910b402a7a651d2e7edc91b
Adriaan <adriaanmarain300@gmail.com>
2020-10-09 11:34:16 +0200

    use named imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 
-import MediaLibraryAttachment from "../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-vue-attachment";
-import MediaLibraryCollection from "../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-vue-collection";
+import { MediaLibraryAttachment } from "../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-vue-attachment";
+import { MediaLibraryCollection } from "../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-vue-collection";
 
 var app = new Vue({
     components: { MediaLibraryAttachment, MediaLibraryCollection },
