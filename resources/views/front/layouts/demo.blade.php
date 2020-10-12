8dcbc360490332b53eb6738eb32cf676ad4eb9ec
Willem Van Bockstal <willem@spatie.be>
2020-10-12 16:34:14 +0200

    Demo layout

--- resources/views/front/layouts/demo.blade.php
+++ resources/views/front/layouts/demo.blade.php
@@ -9,9 +9,9 @@
 
     @include('partials.header', ['compact' => true])    
 
-    <main class="-mt-8 mx-auto max-w-5xl px-4 sm:px-12 flex items-stretch">
+    <main class="-mt-12 md:-mt-8 mx-auto max-w-5xl px-4 sm:px-12 md:flex items-stretch">
         <div>
-            <nav class="sticky top-0 mb-8 -mt-12 -mr-8 bg-blue-50 bg-opacity-50 rounded-sm pl-6 pr-12 py-12 w-64">
+            <nav class="sticky top-0 mb-8 -mt-12 md:-mr-8 bg-blue-50 bg-opacity-50 rounded-sm pl-6 pr-12 py-12 md:w-64">
                 <h3 class="text-xs uppercase tracking-wider font-semibold text-blue-400 pb-4">Pick an example</h3>
                 <ul class="markup-ol text-sm grid gap-2">
                     <li><a href="#" class="font-semibold">Attachment</a></li>
@@ -21,7 +21,7 @@
             </nav>
         </div>
         
-        <section class="flex-grow py-16 px-4 sm:pl-24 sm:pr-16 border-8 border-blue-300 border-opacity-25 rounded-sm
+        <section class="flex-grow py-16 px-4 sm:px-8 md:pl-24 md:pr-16 border-8 border-blue-300 border-opacity-25 rounded-sm
         markup markup-links markup-lists">
             @yield('demo')
         </section>
